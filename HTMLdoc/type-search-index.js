11fd4762bc8e0c04705d34abc29c0d291b004f14
pablo <pabloarmasbosse@gmail.com>
2025-03-16 23:32:08 +0100

    ducumentacion HTML generada

---
+++ HTMLdoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"clases","l":"Curso"},{"p":"clases","l":"Main"},{"p":"clases","l":"Nif"},{"p":"clases","l":"Persona"},{"p":"clases","l":"PersonaTest"}];updateSearchResults();
